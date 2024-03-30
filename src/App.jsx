2e77a465848788fd17df641d6f250b1c9b86072d
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-29 23:54:58 -0500

    Paso1 terminado: añadiendo los useStates en password y email para ver si capta los datos en la consola

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
+
 ///añadiendo estilos emotions y renderiando el login de manera básica sin usar los Hooks de React
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ email, password });
+  }
   return (
     <>
       <h1>Welcome to Poke Collection</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email</label>
           <input
             type="email"
             name="email"
             id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="example@mail.com"
           />
         </div>
@@ -19,6 +29,8 @@ function App() {
             type="password"
             name="password"
             id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="******"
           />
         </div>
