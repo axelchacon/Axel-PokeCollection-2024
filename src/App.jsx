1e3ea377e99c0bd92f78bfb586eb4efa0a177177
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-31 16:51:56 -0500

    Paso2.4: añadiendo el login-form.jsx modifcano el APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,11 @@
-import { useState } from "react";
-import Input from "./components/input";
-import { login } from "./components/services/auto-sevice";
+import LoginForm from "./components/login-form";
 
 ///añadiendo estilos emotions y renderiando el login de manera básica sin usar los Hooks de React
 function App() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-  function handleSubmit(event) {
-    event.preventDefault();
-    // console.log(formData); //{ email: 'axelchacon@mail.com', password:"12344"}
-    login(formData)
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error)); //{id: 2, email: 'axelchacon@mail.com', first_name: 'Axel', last_name: 'Chacon', token: 'Xo5HE99xPtS8c7xdenqTSLWA'}
-  }
-  //handleChange = (e) =>setFormData({ ...formData, password: e.target.value })
-  function handleChange(event) {
-    const { name, value } = event.target;
-    //console.log({name, value}) ==== name:email, value:1234
-    setFormData({ ...formData, [name]: value });
-  }
   return (
     <>
       <h1>Welcome to Poke Collection</h1>
-      <form onSubmit={handleSubmit}>
-        <Input
-          name="email"
-          type="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="example@mail.com"
-          label="Email"
-        />
-        <Input
-          name="password"
-          type="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder="******"
-          label="Password"
-        />
-        <button type="submit">Login</button>
-      </form>
+      <LoginForm />
     </>
   );
 }
---
+++ src/components/login-form.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { login } from "./services/auto-sevice";
+import Input from "./input";
+
+function LoginForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  function handleSubmit(event) {
+    event.preventDefault();
+    // console.log(formData); //{ email: 'axelchacon@mail.com', password:"12344"}
+    login(formData)
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error)); //{id: 2, email: 'axelchacon@mail.com', first_name: 'Axel', last_name: 'Chacon', token: 'Xo5HE99xPtS8c7xdenqTSLWA'}
+  }
+  //handleChange = (e) =>setFormData({ ...formData, password: e.target.value })
+  function handleChange(event) {
+    const { name, value } = event.target;
+    //console.log({name, value}) ==== name:email, value:1234
+    setFormData({ ...formData, [name]: value });
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="example@mail.com"
+          label="Email"
+        />
+        <Input
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="******"
+          label="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
