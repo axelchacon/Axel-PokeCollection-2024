e076cea7b90e623d7b2b2a8da380aade944e333e
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-03-31 16:42:14 -0500

    Paso2.3: enviando al sessionStorage con sessionStorage.setItem(tokenKey, token) el token para que se guarde ahí dentro del archivo auto-service.js

--- src/components/services/auto-sevice.js
+++ src/components/services/auto-sevice.js
@@ -1,6 +1,11 @@
 // auth-service.js
+import { tokenKey } from "../../config";
 import apiFetch from "./api-fetch";
 
 export function login(credentials) {
-  return apiFetch("/login", { body: credentials });
+  return apiFetch("/login", { body: credentials }).then((userData) => {
+    const { token, ...user } = userData;
+    sessionStorage.setItem(tokenKey, token);
+    return user;
+  });
 }
