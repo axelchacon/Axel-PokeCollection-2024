ad910b64aea736ef319de13e65c2f6cd6dd0fe5d
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-04-02 19:21:12 -0500

    Paso3.2: modifando App.js con un condicional parecido a Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import SingupForm from "./components/Singup-Form";
 import LoginForm from "./components/login-form";
 
 ///añadiendo estilos emotions y renderiando el login de manera básica sin usar los Hooks de React
 function App() {
+  const [showLogin, setShowLogin] = useState(true);
+  function handleClick(event) {
+    event.preventDefault();
+    setShowLogin(!showLogin);
+  }
   return (
     <>
       <h1>Welcome to Poke Collection</h1>
-      {/* <LoginForm /> */}
-      <SingupForm />
+      {showLogin ? <LoginForm /> : <SingupForm />}
+      <button onClick={handleClick}>
+        {showLogin ? "Create Account" : "Log in"}
+      </button>
     </>
   );
 }
