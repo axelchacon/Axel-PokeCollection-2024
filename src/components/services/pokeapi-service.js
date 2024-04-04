2f17ecda34e2749d7930d1559a0b69c6ebe2d4f7
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-04-03 19:21:00 -0500

    Paso4.1: en el Searc-Page.jsx se modificó para que funcione más y se creó el archivo pokeapi-services.js para usarlo luego

---
+++ src/components/services/pokeapi-service.js
@@ -0,0 +1,5 @@
+// pokeapi-service.js
+const BASE_URI = "https://pokeapi.co/api/v2/pokemon";
+export function getPokemon(query) {
+  return fetch(`${BASE_URI}/${query}`).then((res) => res.json());
+}
--- src/pages/Search-Page.jsx
+++ src/pages/Search-Page.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import Input from "../components/input";
 function SearchPage() {
-  return <h1>Hola SerachPages</h1>;
+  const [query, setQuery] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(query);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        name="query"
+        placeholder="pokemon name"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button>Search</button>
+    </form>
+  );
 }
 
 export default SearchPage;
