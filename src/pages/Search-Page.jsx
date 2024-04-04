0f3d32d28b45e64dad5c9ec94dddb10a30761164
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-04-03 23:12:33 -0500

    Paso4.4: Dentro del archivo Search-Pages.jsx ya tenermos más formado los datos del pokemon con su botón de favoritos

--- src/pages/Search-Page.jsx
+++ src/pages/Search-Page.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Input from "../components/input";
 import { getPokemon } from "../components/services/pokeapi-service";
+import styled from "@emotion/styled";
 function SearchPage() {
   const [query, setQuery] = useState("");
   const [pokemon, setPokemon] = useState(null);
@@ -16,18 +17,24 @@ function SearchPage() {
     id = String(id);
     return id.length < 2 ? `#00${id}` : id.length < 3 ? `#0${id}` : `#${id}`;
   }
+  const PokeImage = styled.img`
+    max-width: 144px;
+  `;
   function PokemonData({ dataPokemon }) {
     return (
       <div>
         <h2>{dataPokemon.name}</h2>
         <p>{formatId(dataPokemon.id)}</p>
-        <img
+        <PokeImage
           src={dataPokemon.sprites.other["official-artwork"].front_default}
           alt={dataPokemon.name}
         />
         {dataPokemon.types.map((element) => (
           <p key={element.slot}>{element.type.name}</p>
         ))}
+        <p>Height: {dataPokemon.height / 10} m</p>
+        <p>Weight: {dataPokemon.weight / 10} kg</p>
+        <button>Mark as favorite</button>
       </div>
     );
   }
